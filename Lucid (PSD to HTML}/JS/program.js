a749a18ec1435f7b5d7bacc7ccd884d3dbefe1ab
MirzaZamee <85169121+MirzaZamee@users.noreply.github.com>
2021-06-29 13:59:24 +0600

    Add files via upload

---
+++ Lucid (PSD to HTML}/JS/program.js
@@ -0,0 +1,11 @@
+<script>
+$('.owl-carousel').owlCarousel({
+            
+            loop:true,
+            margin:10,
+            items:1,
+            autoplay:true,
+            smartSpeed:400,
+            nav:true,
+          })
+</script>   
